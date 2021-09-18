9afda41e000e71282cf0b787c7a3ccd214345e83
Vinit Saxena <vinit.saxena@telekom-digital.com>
2021-09-18 22:02:46 +0530

    update/main activity ui

--- app/src/main/java/com/sample/translationapp/MainActivity.kt
+++ app/src/main/java/com/sample/translationapp/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.sample.translationapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        text.text = getString(R.string.hello_world)
     }
 }
